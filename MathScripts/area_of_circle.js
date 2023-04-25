630ca8458521575364c9b1bc4634a3c0b97a42bc
Ben Wilhelm <ben.wilhelm@nimblemoose.com>
2023-04-25 12:49:32 -0500

    area of circle

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,10 @@
+// gather inputs
+var radius = 10;
+
+// perform operation
+// area = PI * radius^2
+var area = Math.PI * (radius * radius);
+
+// write output
+console.log('Radius of Circle:', radius);
+console.log('Area of circle', area);
