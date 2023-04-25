acc7626e6513e2e5d9aea76a58d4ca1707cecc36
Ben Wilhelm <ben.wilhelm@nimblemoose.com>
2023-04-25 12:41:31 -0500

    area of rectangle

---
+++ area_of_rectangle.js
@@ -0,0 +1,12 @@
+// gather inputs
+var length = 5;
+var height = 10;
+
+// perform calculation
+// area = length * height
+var area = length * height;
+
+// write outputs
+console.log('Length:', length);
+console.log('Height:', height);
+console.log('Area:  ', area);
